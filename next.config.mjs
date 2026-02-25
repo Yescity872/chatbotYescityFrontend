bf98bd25e12d5c907ebf30d804c79dbabf207646
vaibhav <vaibhav.iiituna1111@gmail.com>
2026-02-25 11:23:15 +0530

    base structure added

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,25 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+<<<<<<< Updated upstream
+=======
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.pinimg.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+>>>>>>> Stashed changes
 };
 
 export default nextConfig;
